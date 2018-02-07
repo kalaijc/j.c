d085748e93cfc77bf29b3119e22474af7f700cc6
kalaijc <35915005+kalaijc@users.noreply.github.com>
2018-02-07 14:13:12 +0530

    kalai.c

---
+++ kalai.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int counter n ;
+    for(counter = 0; counter < n; counter++)
+    {
+      scanf("%d",&n);
+      printf("Hello \n",n);
+    }
+    return 0;
+}
