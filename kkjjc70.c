a4cd5b2e2399e9d03df604e6b53e2ee7a33e7021
kalaijc <35915005+kalaijc@users.noreply.github.com>
2018-03-07 14:47:22 +0530

    Update kkjjc70.c

--- kkjjc70.c
+++ kkjjc70.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main(void)
 {
-	int n,i,a,b,c;
-	printf("enter the number:");
+int a,b,c,n,i;
+printf ("enter the number");
 	scanf("%d",&n);
 	for(i=1;i<=n;i++)
 	{
 		a=pow(2,i);
 		if(a>n)
 		{
-			b=i;
-			break;
+		b=i;
+		break;
 		}
 	}
 	c=pow(2,b);
