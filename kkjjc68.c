7664432d895836eb3cd6db892e81c855c65a8e48
kalaijc <35915005+kalaijc@users.noreply.github.com>
2018-03-07 14:30:07 +0530

    Create kkjjc68.c

---
+++ kkjjc68.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n%7==0)
+	{
+		printf("\n YES");
+	}
+	else
+	{
+		printf("\n NO");
+	}
+	return 0;
+}
