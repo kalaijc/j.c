85441efb4a8082eb2cb1bb42f74ff4da94cff3f4
kalaijc <35915005+kalaijc@users.noreply.github.com>
2018-03-14 16:02:56 +0530

    Create ka3.c

---
+++ ka3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+
+void main(void) 
+
+{
+
+	int n;
+
+	printf("enter the number:");
+
+	scanf("%d",&n);
+
+	if(n%13==0)
+
+	{
+
+		printf("\n yes");
+
+	}
+
+	else
+
+	{
+
+		printf("\n no");
+
+	}
+
+	getch();
+
+}
